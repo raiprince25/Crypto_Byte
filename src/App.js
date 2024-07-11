f47440ac5c75fd53276f92d1659812f152c482a9
Your Name <your_actual_email@example.com>
2024-07-11 11:18:24 +0530

    updated crypto-byte

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { makeStyles } from "@material-ui/core";
+import Homepage from "./Pages/HomePage";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CoinPage from "./Pages/CoinPage";
+import Header from "./components/Header";
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <Router>
+      <div className={classes.App}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/coins/:id" element={<CoinPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
